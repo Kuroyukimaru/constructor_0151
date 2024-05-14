84a9fd2b60bbb4f8d310b2fc2bb300ec6cf3c357
Kuroyukimaru <oranggabut712@gmail.com>
2024-05-14 13:01:37 +0700

    Fungsi cetak untuk mencetak detail mahasiswa

--- construktor2/construktor2.cpp
+++ construktor2/construktor2.cpp
@@ -20,3 +20,18 @@ public:
     Mahasiswa(int iNim, string iNama);
     void cetak();
 };
+
+Mahasiswa::Mahasiswa(string iNama) {
+    nim = 151;
+    nama = iNama;
+}
+
+Mahasiswa::Mahasiswa(int iNim, string iNama) {
+    nim = iNim;
+    nama = iNama;
+}
+
+void Mahasiswa::cetak() {
+    cout << endl << "Nim = " << nim << endl;
+    cout << "Nama = " << nama << endl;
+}
