325ede6e68298fa4c74972b84a5802a89d715afb
Kuroyukimaru <oranggabut712@gmail.com>
2024-05-14 13:00:12 +0700

    membuat publik Konstruktor

--- construktor2/construktor2.cpp
+++ construktor2/construktor2.cpp
@@ -6,4 +6,17 @@ class Mahasiswa {
 private:
     int nim;
     string nama;
+
+public:
+    Mahasiswa() {
+        nim = 151;
+        nama = "";
+    }
+
+    Mahasiswa(int a) {
+        nim = a;
+    }
+    Mahasiswa(string iNama);
+    Mahasiswa(int iNim, string iNama);
+    void cetak();
 };
